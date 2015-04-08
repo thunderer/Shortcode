5ff3de93e4bf5751b9d4a2089b66dedd7a7f0ac7
Tomasz Kowalczyk <tomasz@kowalczyk.cc>
2015-04-08 16:21:17 +0200

    refactored TextSerializer serialization method

--- src/Serializer/TextSerializer.php
+++ src/Serializer/TextSerializer.php
@@ -17,11 +17,19 @@ final class TextSerializer implements SerializerInterface
 
     public function serialize(Shortcode $s)
         {
-        return
-            $this->syntax->getOpeningTag()
-            .$s->getName().$this->serializeParameters($s->getParameters())
-            .$this->syntax->getClosingTag()
-            .(null === $s->getContent() ? '' : $s->getContent().$this->syntax->getOpeningTag().$this->syntax->getClosingTagMarker().$s->getName().$this->syntax->getClosingTag());
+        $open = $this->syntax->getOpeningTag();
+        $close = $this->syntax->getClosingTag();
+        $marker = $this->syntax->getClosingTagMarker();
+
+        $parameters = $this->serializeParameters($s->getParameters());
+        $return = $open.$s->getName().$parameters.$close;
+
+        if(null !== $s->getContent())
+            {
+            $return .= $s->getContent().$open.$marker.$s->getName().$close;
+            }
+
+        return $return;
         }
 
     private function serializeParameters(array $parameters)
@@ -32,9 +40,12 @@ final class TextSerializer implements SerializerInterface
             $return .= ' '.$key;
             if(null !== $value)
                 {
-                $return .= $this->syntax->getParameterValueSeparator().(preg_match('/^\w+$/us', $value)
+                $delimiter = $this->syntax->getParameterValueDelimiter();
+                $separator = $this->syntax->getParameterValueSeparator();
+
+                $return .= $separator.(preg_match('/^\w+$/us', $value)
                     ? $value
-                    : $this->syntax->getParameterValueDelimiter().$value.$this->syntax->getParameterValueDelimiter());
+                    : $delimiter.$value.$delimiter);
                 }
             }
 
